05f66e3e8bcba8800ff07468bb52969b552adf0e
jungle <dgjungle_c@163.com>
2021-09-17 17:57:50 +0800

    master 竞选

--- src/main/java/com/jungle/myzk/Master.java
+++ src/main/java/com/jungle/myzk/Master.java
@@ -1,40 +1,67 @@
 package com.jungle.myzk;
 
 import javafx.concurrent.Worker;
-import org.apache.zookeeper.CreateMode;
-import org.apache.zookeeper.KeeperException;
-import org.apache.zookeeper.ZooDefs;
-import org.apache.zookeeper.ZooKeeper;
+import org.apache.zookeeper.*;
+import org.apache.zookeeper.Watcher.Event.*;
 import org.apache.zookeeper.data.Stat;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
 import java.io.IOException;
 import java.nio.charset.StandardCharsets;
+import java.util.Objects;
 import java.util.Random;
 
-import static org.apache.zookeeper.AsyncCallback.DataCallback;
-import static org.apache.zookeeper.AsyncCallback.StringCallback;
-import static org.apache.zookeeper.KeeperException.Code;
+import static org.apache.zookeeper.AsyncCallback.*;
+import static org.apache.zookeeper.KeeperException.*;
 
 public class Master extends DefaultWatcher {
     public static final Logger LOG = LoggerFactory.getLogger(Worker.class);
-    String serverId = Integer.toHexString(new Random().nextInt());
+    public static final String PATH = "/master";
+    enum MasterStates {RUNNING, ELECTED, NOTELECTED;}
 
-    boolean isLeader;
+    String serverId = Integer.toHexString(new Random().nextInt());
+    volatile MasterStates state = MasterStates.RUNNING;
+    private final Watcher masterExistsWatcher = event -> {
+        if (Objects.equals(event.getType(), EventType.NodeDeleted)) {
+            assert PATH.equals(event.getPath());
+            runForMaster();
+        }
+    };
+    private StatCallback masterExistsCallback = (int rc, String path, Object ctx, Stat stat) -> {
+        switch (Code.get(rc)) {
+            case CONNECTIONLOSS:
+                masterExists();
+                break;
+            case OK:
+                if (stat == null) {
+                    this.state = MasterStates.RUNNING;
+                    runForMaster();
+                }
+                break;
+            default:
+                checkMaster();
+                break;
+        }
+    };
     StringCallback masterCreateCallback = (int rc, String path, Object ctx, String name) -> {
         switch (Code.get(rc)) {
             case OK:
-                isLeader = true;
-                bootStrap();
+                state = MasterStates.ELECTED;
+                takeLeadership();
                 break;
             case CONNECTIONLOSS:
                 checkMaster();
-                return;
+                break;
+            case NODEEXISTS:
+                state = MasterStates.NOTELECTED;
+                masterExists();
+                break;
             default:
-                isLeader = false;
+                state = MasterStates.NOTELECTED;
+                LOG.error("Something went wrong when running for master.", KeeperException.create(Code.get(rc), path));
+
         }
-        System.out.println("I`m " + (isLeader ? "" : "not") + " the leader");
     };
     DataCallback masterCheckCallBack = (int rc, String path, Object ctx, byte[] data, Stat stat) -> {
         switch (Code.get(rc)) {
@@ -66,6 +93,14 @@ public class Master extends DefaultWatcher {
         super(hostPort);
     }
 
+
+    private void takeLeadership() {
+        System.out.println("I`m the master.");
+    }
+
+    private void masterExists() {
+        zk.exists(PATH, masterExistsWatcher, masterExistsCallback, null);
+    }
     public void startZK() throws IOException {
         zk = new ZooKeeper(hostPort, 15000, this);
     }
@@ -74,13 +109,17 @@ public class Master extends DefaultWatcher {
         zk.close();
     }
 
+    public MasterStates getState() {
+        return state;
+    }
+
     public void runForMaster() {
-        zk.create("/master", serverId.getBytes(StandardCharsets.UTF_8),
+        zk.create(PATH, serverId.getBytes(StandardCharsets.UTF_8),
                 ZooDefs.Ids.OPEN_ACL_UNSAFE, CreateMode.EPHEMERAL, masterCreateCallback, null);
     }
 
     public void checkMaster() {
-        zk.getData("/master", false, masterCheckCallBack, null);
+        zk.getData(PATH, false, masterCheckCallBack, null);
     }
 
 
